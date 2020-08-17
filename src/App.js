f760728c2c42456c944caa23554cfd86292cdd3b
unknown <saraiyachandra25496@gmail.com>
2020-08-18 01:40:05 +0530

    prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
-import alanBtn from "@alan-ai/alan-sdk-web";
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
 const alankey =
-  "eb323e171d25e8fbcb18760bcecfb3942e956eca572e1d8b807a3e2338fdd0dc/stage";
+  'eb323e171d25e8fbcb18760bcecfb3942e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
   useEffect(() => {
     alanBtn({
       key: alankey,
       onCommand: ({ command, articles }) => {
-        if (command === "newHeadlines") {
+        if (command === 'newHeadlines') {
           console.log(articles);
         }
       },
