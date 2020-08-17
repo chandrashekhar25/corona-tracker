f17f4cabea00fb14417a366973b059350f5d91d9
unknown <saraiyachandra25496@gmail.com>
2020-08-18 02:03:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 const alankey =
   'eb323e171d25e8fbcb18760bcecfb3942e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
   useEffect(() => {
     alanBtn({
       key: alankey,
       onCommand: ({ command, articles }) => {
         if (command === 'newHeadlines') {
-          console.log(articles);
+          setNewsArticles(articles);
         }
       },
     });
