040011b5b615286bb40ff8388f8176119b40f4c5
unknown <saraiyachandra25496@gmail.com>
2020-08-18 02:30:02 +0530

    hbh

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import { Grid, Grow, Typography } from '@material-ui/core';
 import NewsCard from '../NewsCard/NewsCard';
 
 const NewsCards = ({ articles }) => {
   return (
-    <div>
+    <Grow in>
+      <Grid container alignItems='stretch' spacing={3}></Grid>
       {articles.map((article, i) => (
-        <NewsCard />
+        <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+          <NewsCard article={article} i={i} />
+        </Grid>
       ))}
-    </div>
+    </Grow>
   );
 };
 
