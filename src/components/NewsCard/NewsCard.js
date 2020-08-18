c049c071c8f7ec7fb2cbc8aa2f879373439cc487
unknown <saraiyachandra25496@gmail.com>
2020-08-18 23:39:29 +0530

    k

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import {
+  Card,
+  CardAction,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  CardActions,
+} from '@material-ui/core';
 
-const NewsCard = () => {
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  i,
+}) => {
   return (
-    <div>
-      <h1>News Card</h1>
-    </div>
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          image={urlToImage || 'https://images.app.goo.gl/Bk6hvThkR9XP7Afw7'}
+        />
+        <div>
+          <Typography variant='body2' color='textSecondary' component='h2'>
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant='body2' color='textSecondary' component='h2'>
+            {source.name}
+          </Typography>
+        </div>
+        <Typography gutterBottom variant='h5'>
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography variant='body2' color='textSecondary' component='p'>
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size='small' color='primary'>
+          Learn More
+        </Button>
+        <Typography variant='h5' color='textSecondary'>
+          {i + 1}
+        </Typography>
+      </CardActions>
+    </Card>
   );
 };
 
