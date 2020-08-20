d385ee5320da53733c2edf5aef800143e655b004
unknown <saraiyachandra25496@gmail.com>
2020-08-20 23:09:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
+import wordsToNumbers from 'words-to-numbers';
+import useStyles from './styles.js';
 const alankey =
   'eb323e171d25e8fbcb18760bcecfb3942e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setactiveArticle] = useState(-1);
+  const classes = useStyles();
   useEffect(() => {
     alanBtn({
       key: alankey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === 'newHeadlines') {
           setNewsArticles(articles);
+          setactiveArticle(-1);
+        } else if (command === 'highlight') {
+          setactiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parseNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parseNumber - 1];
+          if (parseNumber > 20) {
+            alanBtn().playText('Please try that again.');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening... ');
+          }
         }
       },
     });
@@ -18,8 +37,14 @@ const App = () => {
 
   return (
     <div>
-      <h1>News App</h1>
-      <NewsCards articles={newsArticles} />
+      <div className={classes.logoContainer}>
+        <img
+          src='https://alan.app/voice/images/previews/preview.jpg'
+          className={classes.alanLogo}
+          alt='alan logo'
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 };
--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -6,5 +6,21 @@ const styles = makeStyles({
     width: '100%',
     margin: 0,
   },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    height: '50vh',
+    padding: '10%',
+    borderRadius: 10,
+    color: 'white',
+  },
+  infoCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+  },
 });
 export default styles;
